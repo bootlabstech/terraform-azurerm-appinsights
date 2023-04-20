13a8b9d70132a573ea5a0492271384ceb51be1b2
lalitha2407 <lalitha@bootlabstech.com>
2023-04-21 00:56:45 +0530

    rewritten with more arguments

--- varilables.tf
+++ varilables.tf
@@ -1,24 +1,73 @@
-variable "location" {
-  description = "The location where the instance will be created in"
-  type        = string
-}
-
 variable "name" {
-  description = "The name of the load test instance"
-  type        = string
+  description = "values"
+  type = string
+  
 }
-
+variable "location" {
+  description = "values"
+  type = string
+  
+}
 variable "resource_group_name" {
-  description = "The resource group where the instance will be created in"
-  type        = string
+  description = "values"
+  type = string
+  
 }
-
 variable "application_type" {
-  description = "Specifies the type of Application Insights to create. Valid values are ios for iOS, java for Java web, MobileCenter for App Center, Node.JS for Node.js, other for General, phone for Windows Phone, store for Windows Store and web for ASP.NET."
-  type        = string
+  description = "values"
+  type = string
+  
 }
-
+variable "daily_data_cap_in_gb" {
+  description = "values"
+  type = string
+  # default = "10"
+  
+}
+variable "daily_data_cap_notifications_disabled" {
+  description = "values"
+  type = bool
+  default = false
+  
+}
+variable "retention_in_days" {
+  description = "values"
+  type = string
+  default = "90"
+  
+}
+variable "sampling_percentage" {
+  description = "values"
+  type = string
+  default = "100"
+  
+}
+variable "disable_ip_masking" {
+  description = "values"
+  type = bool
+  default = false
+  
+}
+variable "local_authentication_disabled" {
+  description = "values"
+  type = bool
+  default = false
+  
+}
+variable "internet_ingestion_enabled" {
+  description = "values"
+  type = bool
+  default = true
+  
+}
+variable "force_customer_storage_for_profiler" {
+  description = "values"
+  type = bool
+  default = false
+  
+}
 # variable "workspace_id" {
-#   description = "Specifies the id of a log analytics workspace resource. Changing this forces a new resource to be created."
-#   type        = string
+#   description = "values"
+#   type = string
+  
 # }
